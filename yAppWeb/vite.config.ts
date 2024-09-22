e541e4a0bff588b268aa21e0c15c23b8d1b4dc60
Qiwen <qzhang0127@gmail.com>
2024-09-22 01:34:36 -0500

    Update vite.config.ts to support .md file

--- yAppWeb/vite.config.ts
+++ yAppWeb/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
+import path, { resolve } from "path";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+	plugins: [vue()],
+
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "src"),
+			"#root": resolve(__dirname),
+			"./runtimeConfig": "./runtimeConfig.browser",
+		},
+	},
+
+	build: {
+		sourcemap: true,
+	},
+	optimizeDeps: {
+		exclude: [],
+	},
+	assetsInclude: ["**/*.md"],
+});
