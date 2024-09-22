5bb9dc33cf78754dc2f4d16646ce5df380ef5501
Qiwen <qzhang0127@gmail.com>
2024-09-22 01:34:02 -0500

    Configure the frontend with library' settings

--- yAppWeb/src/main.ts
+++ yAppWeb/src/main.ts
@@ -1,5 +1,18 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./style.css";
+import { Amplify } from "aws-amplify";
+import { VueQueryPlugin } from "@tanstack/vue-query";
+import awsconfig from "./aws-exports";
+import AmplifyVue from "@aws-amplify/ui-vue";
 
-createApp(App).mount('#app')
+
+Amplify.configure(awsconfig);
+
+const app = createApp(App);
+app.use(router);
+app.use(VueQueryPlugin);
+app.use(AmplifyVue);
+
+app.mount("#app");
