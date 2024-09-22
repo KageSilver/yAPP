55f4994a59f3954d75062580a945b69461820eda
Qiwen <qzhang0127@gmail.com>
2024-09-22 00:20:53 -0500

    Add comments to the LambdaEntryPoints.cs

--- amplify/backend/function/yAppLambda/src/LambdaEntryPoint.cs
+++ amplify/backend/function/yAppLambda/src/LambdaEntryPoint.cs
@@ -1,17 +1,42 @@
 using Amazon.Lambda.AspNetCoreServer;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
 namespace yAppLambda;
 
-public class LambdaEntryPoint : APIGatewayProxyFunction
+/// <summary>
+/// This class extends from APIGatewayProxyFunction which contains the methods FunctionHandlerAsync which is the
+/// actual lambda function entry point. The lambda handler field should be set to
+///
+/// yAppLambda::yAppLambda.LambdaEntryPoint::FunctionHandlerAsync
+/// </summary>
+public class LambdaEntryPoint :
+    // The base class must be set to match the AWS service invoking the lambda function. 
+    // API Gateway REST API                     ->   Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    // API Gateway HTTP API payload version 1.0 ->   Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    // API Gateway HTTP API payload version 2.0 ->   Amazon.Lambda.AspNetCoreServer.APIGatewayHttpApiV2ProxyFunction
+    // Application Load Balancer                ->   Amazon.Lambda.AspNetCoreServer.ApplicationLoadBalancerFunction
+    APIGatewayProxyFunction
 {
+    /// <summary>
+    /// The builder has configuration, logging and Amazon API Gateway already configured. The Startup class
+    /// needs to be configured in this method using the UseStartup<>() method
+    /// </summary>
+    /// <param name="builder"></param>
+    protected override void Init(IWebHostBuilder builder)
+    {
+        builder.UseStartup<Startup>();
+    }
+    
+    /// <summary>
+    /// Use this override to customize the services registered with the IHostBuilder
+    ///
+    /// It is recommended not to call ConfigurationWebHostDefaults to configure the IWebHostBuilder inside this method.
+    /// Instead customized the IWebHostBuilder in the Init(IWebHostBuilder) overload.
+    /// </summary>
+    /// <param name="builder"></param>
     protected override void Init(IHostBuilder builder)
     {
-        builder.ConfigureWebHostDefaults(webBuilder =>
-        {
-            webBuilder.UseStartup<Startup>(); 
-        });
+       
     }
 }
