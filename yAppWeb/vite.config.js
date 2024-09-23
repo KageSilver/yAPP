a0efe97578917cc22c4ecc9d6e9811c70cfd0267
Qiwen <qzhang0127@gmail.com>
2024-09-23 18:49:01 -0500

    Add aws hosting

---
+++ yAppWeb/vite.config.js
@@ -0,0 +1,21 @@
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+import { defineConfig } from "vitest/config";
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "src"),
+            "#root": resolve(__dirname),
+            "./runtimeConfig": "./runtimeConfig.browser",
+        },
+    },
+    build: {
+        sourcemap: true,
+    },
+    optimizeDeps: {
+        exclude: [],
+    },
+    assetsInclude: ["**/*.md"],
+});
