e54cc2694b0a07b267d45db3525eb2d231439d9c
Qiwen <qzhang0127@gmail.com>
2024-09-26 18:16:42 -0500

    Create AppSettings.cs for converting the appsettings.json to the class

--- amplify/backend/function/yAppLambda/src/Models/AppSettings.cs
+++ amplify/backend/function/yAppLambda/src/Models/AppSettings.cs
@@ -1,6 +1,19 @@
 namespace yAppLambda.Models;
 
-public class AppSettings
+public class AppSettings: IAppSettings
 {
-    
+    /// <summary>
+    /// Cloud environment, it should be either test, dev or main
+    /// </summary>
+    public string Environment { get; set; }
+
+    /// <summary>
+    /// The region of the web application
+    /// </summary>
+    public string AwsRegion { get; set; }
+
+    /// <summary>
+    /// Cognito user pool url 
+    /// </summary>
+    public string Cognito { get; set; }
 }
