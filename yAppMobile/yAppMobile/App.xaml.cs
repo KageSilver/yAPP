c8268b09dca508d824b41985cb31b8ac4f1721f8
Qiwen <qzhang0127@gmail.com>
2024-09-25 21:50:56 -0500

    Create the .NET MAUI project called yAppMobile

---
+++ yAppMobile/yAppMobile/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace yAppMobile;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ yAppMobile/yAppMobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace yAppMobile;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
