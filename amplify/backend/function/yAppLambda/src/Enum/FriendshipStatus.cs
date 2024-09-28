1d362ce7aba2d4b9e9f1781024b6a94bc00a358f
Qiwen <qzhang0127@gmail.com>
2024-09-28 13:45:58 -0500

    Add friendship status enum

---
+++ amplify/backend/function/yAppLambda/src/Enum/FriendshipStatus.cs
@@ -0,0 +1,11 @@
+namespace yAppLambda.Enum;
+
+/// <summary>
+/// Represents the status of a friendship.
+/// </summary>
+public enum FriendshipStatus
+{
+    Pending = 0,
+    Accepted = 1,
+    Declined = 2
+}
