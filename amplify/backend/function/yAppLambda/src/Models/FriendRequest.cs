b6d90bf318294de2cdc6c12983d12fa8c69dc258
Qiwen <qzhang0127@gmail.com>
2024-09-28 15:29:13 -0500

    Create friend request model

---
+++ amplify/backend/function/yAppLambda/src/Models/FriendRequest.cs
@@ -0,0 +1,15 @@
+using System.Text.Json.Serialization;
+
+namespace yAppLambda.Models;
+
+/// <summary>
+/// Represents a friend request with the sender's username and the recipient's user ID.
+/// </summary>
+public class FriendRequest
+{
+    [JsonPropertyName("fromUserName")]
+    public string FromUserName { get; set; } 
+    
+    [JsonPropertyName("toUserId")]
+    public string ToUserId { get; set; } 
+}
