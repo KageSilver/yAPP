2c5796338f90fe178ae3e9266d53f67b2978e8c0
Cynthia <calvocyn@gmail.com>
2024-09-30 13:03:23 -0500

    Add post table schema

---
+++ amplify/backend/function/yAppLambda/src/Models/Post.cs
@@ -0,0 +1,25 @@
+namespace yAppLambda.Models;
+
+/// <summary>
+/// Represents a post created by a user
+/// </summary>
+public class Post
+{
+    public string PID { get; set; } // Partition key
+
+    public DateTime CreatedAt { get; set; } // Sort key
+
+    public string UserName { get; set; } // The username of the user who created the post
+
+    public string PostTitle { get; set; } // The title of the post
+
+    public string PostBody {get; set; } // The contents of the post
+
+    public int Upvotes { get; set; } // The number of upvotes the post has
+
+    public int Downvotes { get; set; } // The number of downvotes the post has
+
+    public boolean DiaryEntry { get; set; } // Is the post a diary entry
+
+    public boolean Anonymous { get; set; } // If the post is a diary entry, is it posted anonymously
+}
