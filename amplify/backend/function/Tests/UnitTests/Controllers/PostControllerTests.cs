01bfca6ab043d5e574b68ff74bfa6507f59cd239
Cynthia <calvocyn@gmail.com>
2024-10-01 18:37:51 -0500

    Add PostController unit tests

--- amplify/backend/function/Tests/UnitTests/Controllers/PostControllerTests.cs
+++ amplify/backend/function/Tests/UnitTests/Controllers/PostControllerTests.cs
@@ -28,4 +28,71 @@ public class PostControllerTests
         _postController = new PostController(_mockAppSettings.Object, _mockCognitoActions.Object,
             _mockDbContext.Object, _mockPostActions.Object);
     }
+
+    [Fact]
+    public async Task CreatePost_ShouldReturnBadResult_WhenRequestIsNull()
+    {
+        // Arrange
+        NewPost request = null;
+
+        // Act
+        var result = await _postController.CreatePost(request);
+
+        // Assert
+        var badRequestResult = Assert.IsType<BadRequestObjectResult>(result.Result);
+        Assert.Equal("request body is required and must contain poster's username, post title and post body", badRequestResult.Value);
+    }
+
+    [Fact]
+    public async Task CreatePost_ShouldReturnNotFound_WhenUsernameIsNotFound()
+    {
+        // Arrange
+        var request = new NewPost { UserName = "userDoesNotExist", PostTitle = "title", PostBody = "body", DiaryEntry = false, Anonymous = true };
+        _mockCognitoActions.Setup(c => c.GetUser(request.UserName)).ReturnsAsync((User)null);
+
+        // Act
+        var result = await _postController.CreatePost(request);
+
+        // Assert
+        var notFoundResult = Assert.IsType<NotFoundObjectResult>(result.Result);
+        Assert.Equal("Post creator not found", notFoundResult.Value);
+    }
+
+    [Fact]
+    public async Task CreatePost_ShouldReturnOK_WhenPostIsCreatedSuccessfully()
+    {
+        // Arrange 
+        var request = new NewPost { UserName = "user1@example.com", PostTitle = "title", PostBody = "body", DiaryEntry = false, Anonymous = true };
+        var poster = new User {UserName = "user1@example.com" };
+        var post = new Post 
+        { 
+            UserName = request.UserName, 
+            PostTitle = request.PostTitle, 
+            PostBody = request.PostBody, 
+            Upvotes = 0, 
+            Downvotes = 0, 
+            DiaryEntry = request.DiaryEntry, 
+            Anonymous = request.Anonymous 
+        };
+
+        // Mock GetUser to return the poster
+        _mockCognitoActions.Setup(c => c.GetUser(request.UserName)).ReturnsAsync(poster);
+
+        // Mock CreatePost to return a new Post object
+        _mockPostActions.Setup(p => p.CreatePost(It.IsAny<Post>())).ReturnsAsync(new OkObjectResult(post));
+
+        // Act
+        var result = await _postController.CreatePost(request);
+
+        // Assert
+        var returnedPost = result.Value;
+        // cannot assert PID being equal since PID is randomly generated
+        Assert.Equal(post.UserName, returnedPost.UserName);
+        Assert.Equal(post.PostTitle, returnedPost.PostTitle);
+        Assert.Equal(post.PostBody, returnedPost.PostBody);
+        Assert.Equal(post.Upvotes, returnedPost.Upvotes);
+        Assert.Equal(post.Downvotes, returnedPost.Downvotes);
+        Assert.Equal(post.DiaryEntry, returnedPost.DiaryEntry);
+        Assert.Equal(post.Anonymous, returnedPost.Anonymous);
+    }
 }
