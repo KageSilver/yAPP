acea3770d7e204496b3eb133bfabfb58be42c6f0
Cynthia <calvocyn@gmail.com>
2024-10-01 07:53:52 -0500

    Add dependancy injection to PostActions

--- amplify/backend/function/yAppLambda/src/DynamoDB/PostActions.cs
+++ amplify/backend/function/yAppLambda/src/DynamoDB/PostActions.cs
@@ -6,37 +6,45 @@ using yAppLambda.Models;
 
 namespace yAppLambda.DynamoDB;
 
-public static class PostActions
+public class PostActions : IPostActions
 {
     //This is the default table name for the post table
     private const string PostTableName = "Post-test";
+    private readonly IAppSettings _appSettings;
+    private readonly IDynamoDBContext _dynamoDbContext;
+    private readonly string _postTable;
+    private readonly DynamoDBOperationConfig _config;
+
+    public PostActions(IAppSettings appSettings,IDynamoDBContext dynamoDbContext)
+    {
+        _appSettings = appSettings;
+        _dynamoDbContext = dynamoDbContext;
+
+        _postTable = string.IsNullOrEmpty(_appSettings.PostTableName)
+            ? PostTableName
+            : _appSettings.PostTableName;
+        _config = new DynamoDBOperationConfig
+        {
+            OverrideTableName = _postTable
+        };
+    }
     
     /// <summary>
     /// Creates a post
     /// </summary>
     /// <param name="post">The post object that contains information on the post.</param>
-    /// <param name="dynamoDbContext">The DynamoDB context to interact with the database.</param>
-    /// <param name="appSettings">The application settings containing configuration values.</param>
     /// <returns>An ActionResult containing the created Post object or an error status.</returns>
-   public static async Task<ActionResult<Post>> CreatePost(Post post,
+   public async Task<ActionResult<Post>> CreatePost(Post post,
         IDynamoDBContext dynamoDbContext, IAppSettings appSettings)
     {
         try
         {
-            var postTable = string.IsNullOrEmpty(appSettings.PostTableName)
-                ? PostTableName
-                : appSettings.PostTableName;
-            var config = new DynamoDBOperationConfig
-            {
-                OverrideTableName = postTable
-            };
-
             // update the current time
             post.CreatedAt = DateTime.Now;
             // gets a unique ID for the post
             post.PID = Guid.NewGuid().ToString();
 
-            await dynamoDbContext.SaveAsync(post, config);
+            await _dynamoDbContext.SaveAsync(post, _config);
 
             return new OkObjectResult(post);
 
