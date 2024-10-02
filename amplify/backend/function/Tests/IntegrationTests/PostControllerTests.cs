d85901eef42979e319b491a30a4694a8815848a3
Cynthia <calvocyn@gmail.com>
2024-10-01 19:41:09 -0500

    Add integration tests for CreatePost API endpoint

--- amplify/backend/function/Tests/IntegrationTests/PostControllerTests.cs
+++ amplify/backend/function/Tests/IntegrationTests/PostControllerTests.cs
@@ -10,7 +10,6 @@ using Microsoft.AspNetCore.TestHost;
 using Xunit.Extensions.Ordering;
 using yAppLambda.Common;
 using yAppLambda.DynamoDB;
-using yAppLambda.Enum;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,7 +29,7 @@ public class PostControllerIntegrationTests
     private readonly IAppSettings _appSettings;
 
     //we must use simulator email to test the user without using email quota
-    private const string TestUserEmail1 = "bounce4@simulator.amazonses.com";
+    private const string TestUserEmail = "bounce4@simulator.amazonses.com";
 
     private ICognitoActions _cognitoActions;
     private IPostActions _postActions;
@@ -60,4 +59,91 @@ public class PostControllerIntegrationTests
         _postActions = new PostActions(_appSettings, dynamoDbContext);
     }
 
+    [Fact, Order(1)]
+    public async Task CreatePost_ValidRequest_ReturnsPost()
+    {
+        //setup the user for testing
+        await _cognitoActions.CreateUser(TestUserEmail);
+        await Task.Delay(TimeSpan.FromSeconds(5)); // make sure the user is created
+
+        var responseId = await _client.GetAsync($"/api/users/getUserByName?username={TestUserEmail}");
+        Assert.Equal(HttpStatusCode.OK, responseId.StatusCode);
+        var responseIdString = await responseId.Content.ReadAsStringAsync();
+        var user = JsonConvert.DeserializeObject<User>(responseIdString);
+
+        // Arrange
+        var newPost = new NewPost
+        {
+            UserName = TestUserEmail,
+            PostTitle = "title",
+            PostBody = "body",
+            DiaryEntry = false,
+            Anonymous = true
+        };
+        
+        var content = new StringContent(JsonConvert.SerializeObject(newPost), System.Text.Encoding.UTF8,
+            "application/json");
+
+        // Act
+        var response = await _client.PostAsync("/api/posts/createPost", content);
+        await Task.Delay(TimeSpan.FromSeconds(2)); // Adjust the delay duration as needed
+
+        // Assert
+        var responseString = await response.Content.ReadAsStringAsync();
+
+        var post = JsonConvert.DeserializeObject<Post>(responseString);
+
+        Assert.NotNull(post);
+        Assert.Equal(TestUserEmail, post.UserName);
+        Assert.Equal(newPost.PostTitle, post.PostTitle);
+        Assert.Equal(newPost.PostBody, post.PostBody);
+        Assert.Equal(newPost.DiaryEntry, post.DiaryEntry);
+        Assert.Equal(newPost.Anonymous, post.Anonymous);
+    }
+
+    [Fact, Order(2)]
+    public async Task CreatePost_PosterNotFound_ReturnsNotFound()
+    {
+        // Arrange
+        var newPost = new NewPost
+        {
+            UserName = "userDoesNotExist",
+            PostTitle = "title",
+            PostBody = "body",
+            DiaryEntry = false,
+            Anonymous = true
+        };
+
+        var content = new StringContent(JsonConvert.SerializeObject(newPost), System.Text.Encoding.UTF8,
+            "application/json");
+
+        // Act
+        var response = await _client.PostAsync("/api/posts/createPost", content);
+
+        // Assert
+        Assert.Equal(System.Net.HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact, Order(3)]
+    public async Task CreatePost_InvalidRequest_ReturnsBadRequest()
+    {
+        // Arrange
+        var newPost = new NewPost
+        {
+            UserName = "",
+            PostTitle = "",
+            PostBody = "",
+            DiaryEntry = false,
+            Anonymous = true
+        };
+
+        var content = new StringContent(JsonConvert.SerializeObject(newPost), System.Text.Encoding.UTF8,
+            "application/json");
+
+        // Act
+        var response = await _client.PostAsync("/api/posts/createPost", content);
+
+        // Assert
+        Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+    }
 }
