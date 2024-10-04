1d13967377360a0367a1f133c00967b85dd6239e
kellyshaye <159831015+kellyshaye@users.noreply.github.com>
2024-10-03 22:40:49 -0500

    Modified authenticator form fields + layout

---
+++ yAppWeb/src/components/Authenticator.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import { Authenticator } from "@aws-amplify/ui-vue";
+  import "@aws-amplify/ui-vue/styles.css";
+
+  import { Amplify } from 'aws-amplify';
+  import awsmobile from '../aws-exports';
+
+  Amplify.configure(awsmobile);
+</script>
+
+<template>
+  <h1>FUCK YOU</h1>
+  <authenticator>
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <button @click="signOut">Sign Out</button>
+    </template>
+  </authenticator>
+</template>
