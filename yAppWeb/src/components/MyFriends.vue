1daf6b86d1ea0adc46625f8415d8942e3bcfb484
kellyshaye <159831015+kellyshaye@users.noreply.github.com>
2024-10-08 23:03:39 -0500

    Decode GET response into friends list

--- yAppWeb/src/components/MyFriends.vue
+++ yAppWeb/src/components/MyFriends.vue
@@ -1,6 +1,34 @@
 <script setup>
+    import { get } from 'aws-amplify/api';
+    import { ref, onMounted } from 'vue';
+    import { useAuthenticator } from '@aws-amplify/ui-vue';
+
+    const auth = useAuthenticator();
+    const jsonData = ref([]); // Reacted array to hold the list of friendships
+
+    // Get list of friends as JSON 
+    onMounted(async () => 
+    {
+        try 
+        {
+            const username = auth.user?.username;
+            const restOperation = await get({
+                apiName: 'yapp',
+                path: `/api/friends/getFriendsByStatus?userName=${username}&status=1`
+            });
+            const { body } = await restOperation.response;
+            const response = await ((await body.blob()).arrayBuffer());
+            const decoder = new TextDecoder('utf-8'); // Use TextDecoder to decode the ArrayBuffer to a string
+            const decodedText = decoder.decode(response);
+            jsonData.value = JSON.parse(decodedText); // Update with parsed JSON
+        } 
+        catch(error)
+        {
+            console.log('GET call failed', error);
+        }
+    });
 </script>
 
 <template>
-    <p>Wow...it's kinda empty in here...</p>
+    <li v-for="friendship in jsonData">{{  friendship.ToUserName }}</li>
 </template>
