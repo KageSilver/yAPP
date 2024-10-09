b08abad8f4f3cd77f917cc659a1c0458887b3683
Cynthia <calvocyn@gmail.com>
2024-10-09 10:35:58 -0500

    Add outline for mobile acceptance tests

--- yAppMobile/app/src/androidTest/java/com/example/yappmobile/tests/ProfileManagementAT.java
+++ yAppMobile/app/src/androidTest/java/com/example/yappmobile/tests/ProfileManagementAT.java
@@ -1,4 +1,43 @@
 package com.example.yappmobile.tests;
 
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+
+import com.example.yappmobile.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ProfileManagementAT {
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+    //@Test
+    public void accountCreation()
+    {
+        // TODO
+    }
+
+    //@Test
+    public void accountLogin()
+    {
+        // TODO
+    }
+
+    //@Test
+    public void makeFriends()
+    {
+        // TODO
+    }
+
+    //@Test
+    public void editAccountDetails()
+    {
+        // TODO
+    }
 }
