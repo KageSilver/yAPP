9f5768c6fe145655882fe9bb6333004d0fc6895c
kellyshaye <159831015+kellyshaye@users.noreply.github.com>
2024-10-16 16:02:11 -0500

    Update Authenticator to check session state before reroute

--- yAppMobile/app/src/main/java/com/example/yappmobile/AuthenticatorActivity.java
+++ yAppMobile/app/src/main/java/com/example/yappmobile/AuthenticatorActivity.java
@@ -1,5 +1,6 @@
 package com.example.yappmobile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -9,29 +10,46 @@ import com.amplifyframework.core.Amplify;
 
 public class AuthenticatorActivity extends AppCompatActivity
 {
+    // NOTE: You no longer need to override onActivityResult.
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("Auth", "Before signInWithWebUI");
+        // Check if we need to sign in or not
+        Amplify.Auth.getCurrentUser(
+                result ->
+                {
+                    Log.d("Auth", "There is already a user signed in!");
+                },
+                error ->
+                {
+                    Log.e("Auth", "There is no user that is signed in!");
+                    invokeSignIn();
+                }
+        );
+        Log.i("Re-route", "Rerouting to Home Activity...");
+        startActivity(new Intent(AuthenticatorActivity.this,
+                PublicPostsActivity.class));
+    }
 
+    private void invokeSignIn()
+    {
         // Trigger the Hosted UI sign-in process
         Amplify.Auth.signInWithWebUI(
                 this,
-                result -> {
-                    // Handle success: Redirect to a new screen
+                result ->
+                {
+                    // Handle success
                     Log.d("Auth", "Sign in success");
                 },
-                error -> {
+                error ->
+                {
                     // Handle failure
                     Log.e("AuthQuickStart", "Sign in failed", error);
                 }
         );
-        Log.d("Auth", "After signInWithWebUI");
-
-        //startActivity(new Intent(AuthenticatorActivity.this,PublicPostsActivity.class));
     }
-    // You no longer need to override onActivityResult.
 }
