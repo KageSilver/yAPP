328213aa90db4e5593fd25e7176c620833f735bd
kellyshaye <159831015+kellyshaye@users.noreply.github.com>
2024-10-18 15:44:39 -0500

    Add MyRequestsActivity (before major recycler view refactoring)

--- yAppMobile/app/build.gradle.kts
+++ yAppMobile/app/build.gradle.kts
@@ -30,7 +30,6 @@ android {
         isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
-
     }
     kotlinOptions {
         jvmTarget = "1.8"
--- yAppMobile/app/src/main/java/com/example/yappmobile/MyRequestsActivity.java
+++ yAppMobile/app/src/main/java/com/example/yappmobile/MyRequestsActivity.java
@@ -1,6 +1,15 @@
 package com.example.yappmobile;
 
-public class MyRequestsActivity
-{
+import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyRequestsActivity extends AppCompatActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_requests);
+    }
 }
