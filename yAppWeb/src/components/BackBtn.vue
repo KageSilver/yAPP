85727936af50eeb56be1cea43441c0d1ed3ba0ee
Qiwen <qzhang0127@gmail.com>
2024-10-19 00:56:02 -0500

    Add back button component

---
+++ yAppWeb/src/components/BackBtn.vue
@@ -0,0 +1,17 @@
+<script setup lang="js">
+import { useRouter } from 'vue-router';
+const router = useRouter();
+
+function goBack() {
+    if (props.back) { // Check if back navigation is enabled
+        router.go(-1); // Go back in history
+    }
+}
+</script>
+<template>
+    <div class="mt-5 mb-9">
+        <button class="button icon-button" @click="router.back()">
+            <i class="material-icons text-[3.2rem] text-white">arrow_back</i>
+        </button>
+    </div>
+</template>
