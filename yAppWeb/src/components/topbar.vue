35b3e562840d45275cddf6e69cfc32701594a3ec
Qiwen <qzhang0127@gmail.com>
2024-10-18 23:10:31 -0500

    Create top bar

---
+++ yAppWeb/src/components/topbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div
+        class="fixed top-0 left-0 w-full bg-gradient-to-t from-blue-700 to-blue-500 text-white shadow-md z-50 h-[8rem]">
+        <div class="max-w-7xl mx-auto px-2 sm:px-2 lg:px-2 py-4 flex justify-between items-center h-full">
+            
+            <div class="shrink-0 mr-4">
+                <img src="../assets/yAPP-icon-light.svg" alt="yAPP Logo" class="md:h-[74px] h-10 inline-block">
+                <span class="ml-2 md:text-[48px] text-[20px]">yAPP</span> <!-- Logo text -->
+            </div>
+
+            <!-- Search Bar Container centered with flex-auto to take up available space -->
+            <div class="flex-grow md:mx-[10rem] mx-4">
+                <div class="relative rounded-full bg-white shadow-lg">
+                    <!-- Input Field -->
+                    <input type="text" class="pl-10 pr-16 py-3 w-full rounded-full focus:outline-none text-gray-700"
+                        placeholder="Search">
+                    <!-- Search Icon -->
+                    <div class="absolute top-0 right-0 mt-3 mr-4 text-gray-400">
+                      <span class="material-icons">search</span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</template>
+
