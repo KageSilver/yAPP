228d9f6e2c24a817c72af522ce3245c30e28d6d6
Qiwen <qzhang0127@gmail.com>
2024-10-18 23:25:08 -0500

    Rename TopNav & SideNav

--- yAppWeb/src/views/index.vue
+++ yAppWeb/src/views/index.vue
@@ -1,14 +1,20 @@
+
+<script setup lang="js">
+import SideNav from "../components/SideNav.vue";
+import TopNav from "../components/TopNav.vue";
+</script>
+
 <template>
     <div class="flex h-screen overflow-hidden bg-hero-gradient">
         <div class="flex h-screen overflow-hidden">
             <!-- Sidebar on the right, no padding -->
             <div class="w-64 bg-dark-green h-full">
-                <sidebar />
+                <SideNav />
             </div>
 
             <!-- Main content area on the left, no padding -->
             <div class="flex-1 flex flex-col">
-                <topbar />
+                <TopNav />
                 <!-- Page content -->
                 <main class="flex-1 overflow-y-auto">
                     <router-view></router-view> <!-- Vue Router Outlet -->
@@ -18,7 +24,3 @@
 </div>
 </template>
 
-<script setup lang="ts">
-import sidebar from "../components/sidebar.vue";
-import topbar from "../components/topbar.vue";
-</script>
