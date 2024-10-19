93204627ea243bb19b79c492f44b50e52027dc5c
Qiwen <qzhang0127@gmail.com>
2024-10-18 23:09:17 -0500

    Create the index page

---
+++ yAppWeb/src/views/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex h-screen overflow-hidden bg-hero-gradient">
+        <div class="flex h-screen overflow-hidden">
+            <!-- Sidebar on the right, no padding -->
+            <div class="w-64 bg-dark-green h-full">
+                <sidebar />
+            </div>
+
+            <!-- Main content area on the left, no padding -->
+            <div class="flex-1 flex flex-col">
+                <topbar />
+                <!-- Page content -->
+                <main class="flex-1 overflow-y-auto">
+                    <router-view></router-view> <!-- Vue Router Outlet -->
+                </main>
+            </div>
+        </div>
+</div>
+</template>
+
+<script setup lang="ts">
+import sidebar from "../components/sidebar.vue";
+import topbar from "../components/topbar.vue";
+</script>
