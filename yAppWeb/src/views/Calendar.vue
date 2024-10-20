1fea955a5fde13bcaae06335bf814a1033eea7ce
Cynthia <calvocyn@gmail.com>
2024-10-20 16:00:47 -0500

    Fetch diary entries from user with getPostsByUser endpoint

--- yAppWeb/src/views/Calendar.vue
+++ yAppWeb/src/views/Calendar.vue
@@ -1,7 +1,48 @@
 <script setup>
+import { getCurrentUser } from 'aws-amplify/auth';
+import { onMounted, ref } from 'vue';
+import { get } from 'aws-amplify/api';
+import { useRouter } from 'vue-router';
+
+const router = useRouter(); // Use router hook
+const username = ref('');
+const jsonData = ref([]);
+const loading = false;
+
+// Retrieve the necessary data and function from the helper
+onMounted(async () => {
+    const user = await getCurrentUser();
+    username.value = user.username;
+    await getMyDiaryEntries(username);
+
+});
+
+async function getMyDiaryEntries(username)
+{
+    try
+    {
+        const restOperation = get({
+            apiName: 'yapp',
+            path: `/api/posts/getPostsByUser?userName=${username.value}&diaryEntry=${true}`
+        });
+        const { body } = await restOperation.response;
+        const response = await ((await body.blob()).arrayBuffer());
+        const decoder = new TextDecoder('utf-8'); // Use TextDecoder to decode the ArrayBuffer to a string
+        const decodedText = decoder.decode(response);
+        jsonData.value = JSON.parse(decodedText); // Update with parsed JSON
+    } 
+    catch (e)
+    {
+        console.log('GET call failed', error);
+    }
+}
 
 </script>
 
-<template>
+<template>  
     
 </template>
+
+<style>
+
+</style>
