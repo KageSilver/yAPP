bde27d92ee1598e9b6a49cf3d0eab7c9331b2f0c
Qiwen <qzhang0127@gmail.com>
2024-10-20 13:34:49 -0500

    Remove search bar

--- yAppWeb/src/components/TopNav.vue
+++ yAppWeb/src/components/TopNav.vue
@@ -9,17 +9,17 @@
             </div>
 
             <!-- Search Bar Container centered with flex-auto to take up available space -->
-            <div class="flex-grow md:mx-[10rem] mx-4">
+            <!-- <div class="flex-grow md:mx-[10rem] mx-4">
                 <div class="relative rounded-full bg-white shadow-lg">
-                    <!-- Input Field -->
+                 
                     <input type="text" class="pl-10 pr-16 py-3 w-full rounded-full focus:outline-none text-gray-700"
                         placeholder="Search">
-                    <!-- Search Icon -->
+                
                     <div class="absolute top-0 right-0 mt-3 mr-4 text-gray-400">
                       <span class="material-icons">search</span>
                     </div>
                 </div>
-            </div>
+            </div> -->
 
         </div>
     </div>
