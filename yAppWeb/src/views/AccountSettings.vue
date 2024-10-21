1da6083d0a14bc71cd0908cd62c559abdc896200
Qiwen <qzhang0127@gmail.com>
2024-10-20 21:53:44 -0500

    Adjust padding

--- yAppWeb/src/views/AccountSettings.vue
+++ yAppWeb/src/views/AccountSettings.vue
@@ -1,38 +1,35 @@
 <script setup>
-import { updatePassword } from 'aws-amplify/auth';
+	import {
+		updatePassword
+	} from 'aws-amplify/auth';
 	import BackBtn from '../components/BackBtn.vue';
 
 	// Reference to changing password via auth
 	// https://docs.amplify.aws/gen1/javascript/prev/build-a-backend/auth/manage-passwords/
 
-	function onSubmit() 
-	{
-		const oldPassword= document.getElementById("oldPassword").value;
+	function onSubmit() {
+		const oldPassword = document.getElementById("oldPassword").value;
 		const newPassword = document.getElementById("newPassword").value;
 
-		if(oldPassword !== '' && newPassword !== '') 
-		{
+		if (oldPassword !== '' && newPassword !== '') {
 			handleUpdatePassword(oldPassword, newPassword);
-		} 
-		else 
-		{
+		} else {
 			alert('Please fill in both fields before submitting!');
 		}
 	}
 
-	async function handleUpdatePassword(oldPassword, newPassword) 
-	{
-		try 
-		{
-			await updatePassword({ oldPassword, newPassword });
+	async function handleUpdatePassword(oldPassword, newPassword) {
+		try {
+			await updatePassword({
+				oldPassword,
+				newPassword
+			});
 			alert('Password updated!');
 			router.push('/profile/myposts');
-		} 
-		catch 
-		{
-			alert('Password reset failed!' + 
-					'Please ensure you entered your old password correctly' + 
-					'and that your new password is a minimum of 8 characters');
+		} catch {
+			alert('Password reset failed!' +
+				'Please ensure you entered your old password correctly' +
+				'and that your new password is a minimum of 8 characters');
 
 			document.getElementById("oldPassword").value = '';
 			document.getElementById("newPassword").value = '';
@@ -41,31 +38,33 @@ import { updatePassword } from 'aws-amplify/auth';
 </script>
 
 <template>
-	<div class="px-16 pr-32 pt-[10rem]">
-		<div class="flex justify-between items-center w-full px-16 pr-32">
-			<div class="flex items-center">
-				<BackBtn class="mt-2" />
-				<h1 class="text-white text-4xl font-bold ml-8">Account Settings </h1>
+	<div class="pt-[10rem] md:px-16 md:pr-32 ">
+		<div class="flex flex-col w-full pl-16">
+			<div class="flex items-center mb-0">
+				<BackBtn class="mt-2 mb-0" />
+				<h1 class="text-white text-4xl font-bold ml-8 mb-0">Account Settings</h1>
 			</div>
+			<p class="text-white text-sm font-bold m-0 pl-20">Want to change your password?</p>
 		</div>
-		<div class="flex justify-start items-center w-full px-16 pr-32">
-			<div class="flex items-center px-16">
-				<h1 class="text-white text-sm font-bold ml-5">want to change your password ?</h1>
+
+
+		<div class="w-full md:px-16 md:mx-6 mt-3">
+			<div class="post-heading bg-white p-8 rounded-lg">
+			<div class="form-group w-full mb-4">
+				<label for="title" class="mb-5 font-bold">Old Password:</label>
+				<input required id="oldPassword" type="password"
+					class="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500">
 			</div>
-		</div>
-		<div class="bg-white text-dark p-16 rounded-xl mx-10">
-			<div class="fieldset">
-				<label class="mb-5 font-bold">Old Password: </label>
-				<input class="input border border-dark text-xl  mb-2 w-full" id="oldPassword" type="password"
-					placeholder="Password">
+			<div class="form-group w-full mb-4">
+				<label for="title" class="mb-5 font-bold">New Password:</label>
+				<input required id="newPassword" type="password"
+					class="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500">
 			</div>
-			<div class="fieldset">
-				<label class="mb-5 font-bold">New Password: </label>
-				<input class="input border border-dark text-xl mb-2 w-full" id="newPassword" type="password"
-					placeholder="New Password">
-			</div>
+
 			<br>
-			<button class="bg-dark text-white p-2 rounded-xl w-full" @click="onSubmit">Submit Changes</button>
+			<button class="bg-pink-purple text-white px-5 py-3 rounded-xl w-full" @click="onSubmit">Submit
+				Changes</button>
+			</div>
 		</div>
 	</div>
 </template>
@@ -78,5 +77,4 @@ import { updatePassword } from 'aws-amplify/auth';
 		flex-direction: column;
 		justify-content: flex-start;
 	}
-	
 </style>
