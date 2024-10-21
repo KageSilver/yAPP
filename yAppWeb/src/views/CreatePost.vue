754ff4cc1192564ddd288348c6ceb24bee0e572c
Cynthia <calvocyn@gmail.com>
2024-10-21 17:07:08 -0500

    Re-format diary entry and anonymous toggles

--- yAppWeb/src/views/CreatePost.vue
+++ yAppWeb/src/views/CreatePost.vue
@@ -18,8 +18,9 @@ import BackBtnHeader from "../components/BackBtnHeader.vue";
 	const jsonData = ref([]);
 	const loading = false;
 
-    var diaryEntry = false;
-    var anonymous = true;
+    const diaryEntryIsChecked = ref(false);
+    const anonIsChecked = ref(true);
+
     // Retrieve the necessary data and function from the helper
     onMounted(async () => {
         const user = await getCurrentUser();
@@ -42,8 +43,8 @@ import BackBtnHeader from "../components/BackBtnHeader.vue";
 		createButton.disabled = true;
 		newPost.postTitle = postElements[0].value;
 		newPost.postBody = postElements[1].value;
-		newPost.diaryEntry = diaryEntry;
-		newPost.anonymous = anonymous;
+		newPost.diaryEntry = diaryEntryIsChecked.value;
+		newPost.anonymous = anonIsChecked.value;
 
 		if (diaryEntry) {
 			checkDiaryEntryLimit();
@@ -109,19 +110,23 @@ import BackBtnHeader from "../components/BackBtnHeader.vue";
 	// This function is used for whether we want to show the anonymous toggle
 	// Modify the diary entry and anonymous values here
 	function toggleDiaryEntry() {
-		diaryEntry = !diaryEntry;
+
+        diaryEntryIsChecked.value = !diaryEntryIsChecked.value;
 		var anonymousToggle = document.getElementById("anonymous");
+		var anonymousText = document.getElementById("anonymousText");
+
 		if ( anonymousToggle.hidden == true ) {
 			anonymousToggle.hidden = false;
-			anonymous = false;
+			anonymousText.hidden = false;
 		} else {
 			anonymousToggle.hidden = true;
-			anonymous = true;
+			anonymousText.hidden = true;
+            anonIsChecked.value = true;
 		}
 	}
 
 	function toggleAnonymous(){
-		anonymous = !anonymous;
+        anonIsChecked.value = !anonIsChecked.value;
 	}
 
 	function checkDiaryEntryLimit(){
@@ -134,9 +139,58 @@ import BackBtnHeader from "../components/BackBtnHeader.vue";
 	<div class="backBtnDiv">
 		<BackBtnHeader header="Create a new post!" subheader="Yapp your heart out."  />
 
-
 		<div class="w-full md:px-16 md:mx-6 mt-3">
+
 			<form class="post-heading bg-white p-8 rounded-lg" id="post">
+
+                <form class="border-2 border-gray-300 p-8 rounded-lg mb-4">
+                    
+                    <div class="mb-4 float-root">
+                        <label class="float-left block text-gray-700 text-lg font-semibold">Diary Post?</label>
+
+                        <label class="float-right cursor-pointer select-none items-center">
+                            <div class="relative ml-2 mr-2">
+                                <input type="checkbox" class="sr-only" @change="toggleDiaryEntry" />
+                                <div
+                                    :class="{ '!bg-[#A55678]': diaryEntryIsChecked }"
+                                    class="block h-8 rounded-full box bg-[#9E9E9E] w-14"
+                                ></div>
+                                <div
+                                    :class="{ 'translate-x-full': diaryEntryIsChecked }"
+                                    class="dot absolute left-1 top-1 h-6 w-6 rounded-full bg-white transition"
+                                ></div>
+                            </div>
+                        </label>
+                    </div>
+
+                    <div>
+                        <label class="block text-gray-700 mb-2 mt-8">Diary entry posts will only be shown to your friends</label>
+                    </div>
+
+                    <div hidden id="anonymous" class="mb-4 float-root">
+                        <label class="float-left block text-gray-700 text-lg font-semibold">Anonymous?</label>
+
+                        <label class="float-right cursor-pointer select-none items-center">
+                            <div class="relative ml-2 mr-2">
+                                <input type="checkbox" class="sr-only" @change="toggleAnonymous" />
+                                <div
+                                    :class="{ '!bg-[#A55678]': anonIsChecked }"
+                                    class="block h-8 rounded-full box bg-[#9E9E9E] w-14"
+                                ></div>
+                                <div
+                                    :class="{ 'translate-x-full': anonIsChecked }"
+                                    class="dot absolute left-1 top-1 h-6 w-6 rounded-full bg-white transition"
+                                ></div>
+                            </div>
+                        </label>
+                    </div>
+
+                    <div hidden id="anonymousText">
+                        <label class="block text-gray-700 mb-2 mt-10">Anonymous diary posts will not show your username to your friends</label>
+                    </div>
+
+                </form>
+
 				<div class="form-group w-full mb-4">
 					<label for="title" class="block mb-2 text-gray-700">Title:</label>
 					<input type="text" id="title" required placeholder="Insert your title here."
@@ -149,25 +203,6 @@ import BackBtnHeader from "../components/BackBtnHeader.vue";
 						placeholder="Insert the most heinous, confounding, baffling tea you've ever heard."
 						class="input"></textarea>
 				</div>
-
-				<div class="mb-4">
-					<label class="block text-gray-700 mb-2">Diary Post?</label>
-					<label class="switch">
-						<input type="checkbox" @click="toggleDiaryEntry">
-						<span class="slider round"></span>
-					</label>
-					<text>Diary posts are only visible to your friends</text>
-				</div>
-
-				<div hidden id="anonymous" class="mb-4">
-					<label class="block text-gray-700 mb-2">Anonymous?</label>
-					<label class="switch">
-						<input type="checkbox" @click="toggleAnonymous">
-						<span class="slider round"></span>
-					</label>
-					<text>Anonymous diary posts will not show your username to your friends</text>
-				</div>
-
 
 				<div class="flex flex-col space-y-2 w-full">
 					<button title="Create Post" id="create-button"
