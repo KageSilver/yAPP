0db7c78fb5ee621d1356a5ebf589a624296ae1ba
kellyshaye <159831015+kellyshaye@users.noreply.github.com>
2024-10-22 09:50:50 -0500

    Add user feedback to ResetPassword

--- yAppMobile/app/src/main/java/com/example/yappmobile/ProfileManagement/AddFriendActivity.java
+++ yAppMobile/app/src/main/java/com/example/yappmobile/ProfileManagement/AddFriendActivity.java
@@ -51,11 +51,13 @@ public class AddFriendActivity extends AppCompatActivity
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after)
             {
+
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count)
             {
+
             }
 
             @Override
--- yAppMobile/app/src/main/java/com/example/yappmobile/ProfileManagement/ResetPasswordActivity.java
+++ yAppMobile/app/src/main/java/com/example/yappmobile/ProfileManagement/ResetPasswordActivity.java
@@ -3,9 +3,12 @@ package com.example.yappmobile.ProfileManagement;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AlertDialog;
@@ -14,12 +17,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.amplifyframework.core.Amplify;
 import com.example.yappmobile.NaviBarDestinations.ProfileActivity;
 import com.example.yappmobile.R;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.concurrent.CompletableFuture;
 
 public class ResetPasswordActivity extends AppCompatActivity
 {
     private AlertDialog success;
     private AlertDialog failure;
     private AlertDialog confirm;
+    private TextInputLayout oldPass;
+    private TextInputLayout newPass;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -38,8 +46,57 @@ public class ResetPasswordActivity extends AppCompatActivity
         initializeFailureDialog();
         initializeConfirmDialog();
 
-        String oldPassword = findViewById(R.id.old_password).toString();
-        String newPassword = findViewById(R.id.new_password).toString();
+        oldPass = findViewById(R.id.old_password);
+        newPass = findViewById(R.id.new_password);
+        EditText oldPassEditText = oldPass.getEditText();
+        EditText newPassEditText = newPass.getEditText();
+        oldPassEditText.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+                oldPass.setError(null);
+            }
+        });
+        newPassEditText.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+                final int MIN_CHAR = 6;
+                if(s.length() < MIN_CHAR)
+                {
+                    newPass.setError("Minimum 8 characters");
+                }
+                else
+                {
+                    newPass.setError(null);
+                }
+            }
+        });
 
         // Set up code for back button
         ImageButton backButton = findViewById(R.id.back_button);
@@ -48,7 +105,8 @@ public class ResetPasswordActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                if (!isEmptyFields(oldPassword, newPassword))
+                if (isFilled(oldPassEditText.getText().toString(),
+                             newPassEditText.getText().toString()))
                 {
                     Intent intent = new Intent(ResetPasswordActivity.this, ProfileActivity.class);
                     startActivity(intent);
@@ -67,11 +125,9 @@ public class ResetPasswordActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                invokeUpdatePass(oldPassword, newPassword);
-                // TODO figure out a way to show appropriate dialog
-                // theres something weird with the main thread.
-                // apparently you can't show the dialog alert in the amplify api call
-                // TODO also configure user access to let them update their own password
+                invokeUpdatePass(oldPassEditText.getText().toString(),
+                                 newPassEditText.getText().toString());
+                // TODO: Configure user access to let them update their own password
             }
         });
     }
@@ -81,7 +137,8 @@ public class ResetPasswordActivity extends AppCompatActivity
         // Create failed password alert
         success = new AlertDialog.Builder(this).create();
         success.setTitle("Successfully reset password!");
-        success.setButton(AlertDialog.BUTTON_POSITIVE, "Heck yeah", new DialogInterface.OnClickListener()
+        success.setButton(AlertDialog.BUTTON_POSITIVE,
+                          "Heck yeah", new DialogInterface.OnClickListener()
         {
             public void onClick(DialogInterface dialog, int id)
             {
@@ -95,8 +152,7 @@ public class ResetPasswordActivity extends AppCompatActivity
         // Create failed password alert
         failure = new AlertDialog.Builder(this).create();
         failure.setTitle("Failed to reset password...Try again!");
-        failure.setMessage("Please ensure you enter your old password correctly "
-                           + "AND have a minimum of 8 characters in your new password");
+        failure.setMessage("Please ensure you enter your old password correctly");
         failure.setButton(AlertDialog.BUTTON_POSITIVE, "Aw man...", new DialogInterface.OnClickListener()
         {
             public void onClick(DialogInterface dialog, int id)
@@ -112,14 +168,16 @@ public class ResetPasswordActivity extends AppCompatActivity
         confirm = new AlertDialog.Builder(this).create();
         confirm.setTitle("Woah there!");
         confirm.setMessage("You still have some unsaved changes. Are you sure you want to discard them?");
-        confirm.setButton(AlertDialog.BUTTON_POSITIVE, "Yes", new DialogInterface.OnClickListener()
+        confirm.setButton(AlertDialog.BUTTON_POSITIVE,
+                          "Yes", new DialogInterface.OnClickListener()
         {
             public void onClick(DialogInterface dialog, int id)
             {
                 startActivity(new Intent(ResetPasswordActivity.this, ProfileActivity.class));
             }
         });
-        confirm.setButton(AlertDialog.BUTTON_NEGATIVE, "No", new DialogInterface.OnClickListener()
+        confirm.setButton(AlertDialog.BUTTON_NEGATIVE,
+                          "No", new DialogInterface.OnClickListener()
         {
             public void onClick(DialogInterface dialog, int id)
             {
@@ -128,29 +186,43 @@ public class ResetPasswordActivity extends AppCompatActivity
         });
     }
 
-    private void invokeUpdatePass(String oldPass, String newPass)
+    private void invokeUpdatePass(String oldPassword, String newPassword)
     {
-        if (!isEmptyFields(oldPass, newPass))
+        if (isFilled(oldPassword, newPassword))
         {
-            Amplify.Auth.updatePassword(oldPass,
-                                        newPass, () -> Log.i("Auth", "Successful password update!"),
-                                        error -> Log.e("Auth", "Failed password update...", error));
+            CompletableFuture<Boolean> future = new CompletableFuture<>();
+            Amplify.Auth.updatePassword(oldPassword, newPassword,
+                                        () -> future.complete(true),
+                                        error -> future.complete(false)
+                                       );
+            future.thenAccept(successful -> {
+                runOnUiThread(() -> {
+                    if(successful)
+                    {
+                        success.show();
+                    }
+                    else
+                    {
+                        failure.show();
+                    }
+                });
+            });
         }
-        else if (oldPass.isEmpty())
+        else if (oldPassword.isEmpty())
         {
-            // TODO
-            Log.i("FormField", "Old password is required!!");
+            Log.d("Fault form field", "Old password is required!!");
+            oldPass.setError("Old password is required!");
         }
         else
         {
-            // TODO
-            Log.i("FormField", "New password is required!!");
+            Log.d("Faulty form field", "New password is required!!");
+            newPass.setError("New password is required!");
         }
     }
 
-    private boolean isEmptyFields(String oldPass, String newPass)
+    private boolean isFilled(String oldPass, String newPass)
     {
         // See if we can safely go back to previous page
-        return oldPass.trim().isEmpty() && newPass.trim().isEmpty();
+        return !oldPass.equals("") && !newPass.equals("");
     }
 }
