a386d5665e2a9e953101fb8fc1927426e15f4c35
kellyshaye <159831015+kellyshaye@users.noreply.github.com>
2024-10-24 21:54:42 -0500

    Remove old reset password and modify it to link to website

--- yAppMobile/app/src/main/java/com/example/yappmobile/ProfileManagement/ResetPasswordActivity.java
+++ yAppMobile/app/src/main/java/com/example/yappmobile/ProfileManagement/ResetPasswordActivity.java
@@ -1,10 +1,15 @@
 package com.example.yappmobile.ProfileManagement;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.yappmobile.NaviBarDestinations.ProfileActivity;
 import com.example.yappmobile.R;
 
 public class ResetPasswordActivity extends AppCompatActivity
@@ -19,5 +24,19 @@ public class ResetPasswordActivity extends AppCompatActivity
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
         setContentView(R.layout.activity_reset_password);
+
+        TextView link = findViewById(R.id.redirect_button);
+        link.setMovementMethod(LinkMovementMethod.getInstance());
+
+        ImageButton backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(ResetPasswordActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
