c8fa9806cdcbb7fa9cdd0b7baecd59025241dde0
Cynthia <calvocyn@gmail.com>
2024-10-25 15:52:45 -0500

    Add files for mobile calendar view

---
+++ yAppMobile/app/src/main/java/com/example/yappmobile/NaviBarDestinations/CalendarActivity.java
@@ -0,0 +1,14 @@
+package com.example.yappmobile.NaviBarDestinations;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CalendarActivity extends AppCompatActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
