78e1ebf583ab90d7b69eb2fde96588189752e4b6
Qiwen <qzhang0127@gmail.com>
2024-10-25 19:42:00 -0500

    Add update post function

--- yAppWeb/src/views/PostDetails.vue
+++ yAppWeb/src/views/PostDetails.vue
@@ -1,28 +1,35 @@
 <script setup>
     import {
-        get
-    } from 'aws-amplify/api';
-    import {
-        getCurrentUser
-    } from 'aws-amplify/auth';
-    import {
-        onMounted,
-        ref
-    } from 'vue';
-    import {
-        useRoute,
-        useRouter
-    } from 'vue-router';
-    import BackBtn from '../components/BackBtn.vue';
-    import ConfirmationModal from '../components/ConfirmationModal.vue';
-    import DotMenu from '../components/DotMenu.vue';
+    get,
+    put
+} from 'aws-amplify/api';
+import {
+    getCurrentUser
+} from 'aws-amplify/auth';
+import {
+    onMounted,
+    ref
+} from 'vue';
+import {
+    useRoute
+} from 'vue-router';
+import Alert from '../components/Alert.vue';
+import BackBtn from '../components/BackBtn.vue';
+import ConfirmationModal from '../components/ConfirmationModal.vue';
+import DotMenu from '../components/DotMenu.vue';
 
     const route = useRoute();
-    const post = ref(null);
+    const currentPost = ref(null);
     //post editing and deleting
     const isEditing = ref(false); // State to control the visibility of the edit form
     const isDeleting = ref(false); // State to control the visibility of the confirmation modal
     const isDiscardingUpdate = ref(false); // State to control the visibility of the confirmation modal
+    const showAlert = ref(false);
+    const alertMsg = ref({
+        header: '',
+        message: ''
+        });
+
 
 
     const isAddingComment = ref(false); // State to control the visibility of the adding comment form
@@ -35,7 +42,7 @@
         return ref(commentIds.value.includes(comment.cid)).value;
     };
 
-    
+
 
     // Function to handle editing the post
     const editPost = () => {
@@ -52,11 +59,13 @@
         isDeleting.value = false;
     };
 
+    const closeAlert = () => {
+        showAlert.value = false;
+    };
+
     const confirmDelete = () => {
         // Make API call to delete the post
         //get the updated values
-
-
     };
 
 
@@ -126,113 +135,117 @@
         userId.value = user.userId;
 
         try {
-            // const restOperation = await get({
-            //     apiName: 'yapp',
-            //     path: `/api/posts/getPostById?pid=${pid}`,
-            // });
-            // const {
-            //     body
-            // } = await restOperation.response;
-            // const response = await ((await body.blob()).arrayBuffer());
-            // const decoder = new TextDecoder('utf-8');
-            // const decodedText = decoder.decode(response);
-            // post.value = JSON.parse(decodedText);
-
-            // if (post.value.uid === user.userId) {
-            //     isEditable.value = true;
-            // } else {
-            //     isEditable.value = false;
-            // }
-     
-
-            //set post value for now
-            post.value = {
-                "pid": 1,
-                "postTitle": "This is a post",
-                "postBody": "This is a post body",
-                "createdAt": "2021-10-10T10:10:10",
-                "updatedAt": "2021-10-10T10:10:10",
-                "uid": user.userId,
-            };
-
-            // also getting a list of the comments for the post
-            comments.value.push({
-                    "cid": 1,
-                    "pid": 1,
-                    "commentBody": "This is a comment",
-                    "createdAt": "2021-10-10T10:10:10",
-                    "updatedAt": "2021-10-10T10:10:10",
-                    "commentBody": "This is a comment",
-                    "upvotes": 10,
-                    "downvotes": 5,
-                    "uid": user.userId, // this allow to edit looks like
-                }, {
-                    "cid": 2,
-                    "pid": 1,
-                    "commentBody": "This is another comment",
-                    "createdAt": "2021-10-10T10:10:10",
-                    "updatedAt": "",
-                    "commentBody": "This is another comment",
-                    "upvotes": 10,
-                    "downvotes": 5,
-                    "uid": user.userId, // this allow to edit looks like
-                },
-
-                {
-                    "cid": 3,
-                    "pid": 1,
-                    "commentBody": "This is another comment",
-                    "createdAt": "2021-10-10T10:10:10",
-                    "updatedAt": "2021-10-10T10:10:10",
-                    "commentBody": "This is another comment",
-                    "upvotes": 10,
-                    "downvotes": 5,
-                    "uid": "123", // this allow to edit looks like
-                }
+            const restOperation = await get({
+                apiName: 'yapp',
+                path: `/api/posts/getPostById?pid=${pid}`,
+            });
+            const {
+                body
+            } = await restOperation.response;
+            const response = await ((await body.blob()).arrayBuffer());
+            const decoder = new TextDecoder('utf-8');
+            const decodedText = decoder.decode(response);
+            currentPost.value = JSON.parse(decodedText);
 
 
-            );
+
+            const restOperationComments = await get({
+                apiName: 'yapp',
+                path: `/api/comments/getCommentsByPid?pid=${pid}`,
+            });
+            const {
+                body: bodyComments
+            } = await restOperationComments.response;
+            const responseComments = await ((await bodyComments.blob()).arrayBuffer());
+            const decoderComments = new TextDecoder('utf-8');
+            const decodedTextComments = decoderComments.decode(responseComments);
+            comments.value = JSON.parse(decodedTextComments);
+
+
         } catch (error) {
             console.log('Failed to load post', error);
         }
     });
 
 
-    const updatePost = () => {
+
+
+
+    const updatePost = async () => {
         // Make API call to update the post
         //get the updated values
-        var updatedPost = {
-            "pid": post.value.pid,
-            "postTitle": document.getElementById("title").value,
-            "postBody": document.getElementById("content").value,
-            "updatedAt": '',
-            "createdAt": post.value.createdAt,
-            "userName": post.value.userName
-        };
-    };
+        var title = document.getElementById("title").value;
+        var content = document.getElementById("content").value;
+        // verify if the title and content are not empty
+        if (title === '' || content === '') {
+            alert('Title and Content cannot be empty');
+            return;
+        }
+        //get current post 
+        const updatedPost = ref(null);
+        updatedPost.value = currentPost.value;
+
+
+        updatedPost.value.postTitle = document.getElementById("title").value;
+        updatedPost.value.postBody = document.getElementById("content").value;
+
+        // Make API call to update the post
+        try {
+
+            const putRequest = await put({
+                apiName: 'yapp',
+                path: '/api/posts/updatePost',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                options: {
+                    body: updatedPost.value
+                }
+            });
+            const {
+                body
+            } = await putRequest.response;
+            const response = await body.json();
+            console.log('PUT call succeeded: ', response);
+       
+            alertMsg.value.header = "Yipee!";
+            alertMsg.value.message = "Update post successfully";
+            showAlert.value = true;
+            // Update the current post with the new values
+            currentPost.value = updatedPost.value;
+            isEditing.value = false; // Set the editing state to false
+            
+            
+        } catch (error) {
+            alertMsg.value.header = "Yipee!";
+            alertMsg.value.message = "Failed to update post! Please try again";
+            console.log('PUT call failed: ', JSON.parse(error.response.body));
+        }
+
+    }
 </script>
 
 <template>
-    <div class="flex flex-row items-start justify-center min-h-screen gap-4 mb-5 px-4 pt-[10rem]">
+    <div class="flex flex-row items-start justify-center min-h-screen gap-4 mb-5 px-4 pt-[10rem]" id="postDetails">
 
 
-        <div v-if="post&&!isEditing" class="w-full justify-center flex">
+        <div v-if="currentPost&&!isEditing" class="w-full justify-center flex">
             <BackBtn class="self-start mt-2" />
             <div
                 class="flex-1 max-w-4xl bg-gray-100 border border-gray-300 rounded-lg shadow transition-shadow hover:shadow-md p-5 m-2">
                 <div class="mb-4 relative">
                     <!-- Post Title and Created At -->
-                    <h3 class="text-lg font-semibold break-words">{{ post.postTitle }}</h3>
+                    <h3 class="text-lg font-semibold break-words">{{ currentPost.postTitle }}</h3>
                     <p class="text-sm text-gray-600">
-                        <strong>Created At:</strong> {{ new Date(post.createdAt).toLocaleString() }}
+                        <strong>Created At:</strong> {{ new Date(currentPost.createdAt).toLocaleString() }}
                     </p>
-                    <DotMenu v-if="post.uid == userId" @edit="editPost" @openDeleteModal="openDeleteModal" />
+                    <DotMenu v-if="currentPost.uid == userId" @edit="editPost" @openDeleteModal="openDeleteModal" />
 
                 </div>
-                
+
                 <!-- Post Body -->
-                <div class="text-gray-700 mb-4 whitespace-pre-wrap break-words">{{ post.postBody }}</div>
-                    <!-- Icons for upvote, downvote, and reply -->
+                <div class="text-gray-700 mb-4 whitespace-pre-wrap break-words">{{ currentPost.postBody }}</div>
+                <!-- Icons for upvote, downvote, and reply -->
                 <div class="flex justify-end space-x-4 mx-4">
                     <!-- <button @click.stop="upvote(post.pid)" class="flex items-center space-x-2">
                         <img src="../assets/post/upvote.svg" alt="Upvote" class="w-6 h-6">
@@ -242,7 +255,8 @@
                         <img src="../assets/post/downvote.svg" alt="Downvote" class="w-6 h-6">
                         <span>20</span>
                     </button> -->
-                    <button @click="reply()" class="flex items-center space-x-2 p-2 rounded-xl hover:bg-light-pink hover:text-white">
+                    <button @click="reply()"
+                        class="flex items-center space-x-2 p-2 rounded-xl hover:bg-light-pink hover:text-white">
                         <img src="../assets/post/reply.svg" alt="Reply" class="w-6 h-6">
                         <span>Reply</span>
                     </button>
@@ -251,7 +265,7 @@
                 <div class="mb-4 relative  mt-5" v-if="isAddingComment">
                     <div class="px-2">
                         <textarea type="text" class="input" placeholder="Add a comment" />
-                    </div>
+                        </div>
                     <div class="flex justify-end p-2">
                             <button class="bg-light-pink p-2 rounded-lg text-white  mr-2"
                                 :id="`cancelAddComment`" @click="canceReply()">Cancel</button>
@@ -279,58 +293,58 @@
 
                             <textarea :id="`commentText-${comment.cid}`" v-if="isEditingComment(comment)&& comment.uid == userId"
                                 class="input">{{ comment.commentBody }}</textarea>
-                        
-                            <DotMenu v-if="!isEditingComment(comment) && comment.uid == userId" :id="`commentDot-${comment.cid}`"
-                                @edit="editComment(comment)" @openDeleteModal="openDeleteCommentModal(comment)" />
 
-                        </div>
-                        <div class="flex justify-end" v-if="isEditingComment(comment)&& comment.uid == userId">
-                            <button class="bg-light-pink p-2 rounded-lg text-white  mr-2"
-                                :id="`cancelComment-${comment.cid}`" @click="cancelEditComment(comment)">Cancel</button>
-                            <button class="bg-dark-pink p-2 rounded-lg text-white "
-                                :id="`updateComment-${comment.cid}`">Update Comment</button>
-                        </div> 
+                        <DotMenu v-if="!isEditingComment(comment) && comment.uid == userId"
+                            :id="`commentDot-${comment.cid}`" @edit="editComment(comment)"
+                            @openDeleteModal="openDeleteCommentModal(comment)" />
+
+                    </div>
+                    <div class="flex justify-end" v-if="isEditingComment(comment)&& comment.uid == userId">
+                        <button class="bg-light-pink p-2 rounded-lg text-white  mr-2"
+                            :id="`cancelComment-${comment.cid}`" @click="cancelEditComment(comment)">Cancel</button>
+                        <button class="bg-dark-pink p-2 rounded-lg text-white "
+                            :id="`updateComment-${comment.cid}`">Update Comment</button>
                     </div>
                 </div>
-
-            </div>
-
-
-        </div>
-
-        <div v-if="post&&isEditing"
-            class="flex-1 max-w-4xl bg-gray-100 border border-gray-300 rounded-lg shadow transition-shadow hover:shadow-md p-5 m-2">
-            <div class="form-group w-full mb-4">
-                <label for="title" class="block mb-2 text-gray-700">Title:</label>
-                <input type="text" id="title" required :value="post.postTitle" class="input">
-            </div>
-            <div class="form-group w-full mb-4">
-                <label for="content" class="block mb-2 text-gray-700">Content:</label>
-                <textarea id="content" required :value="post.postBody" class="input"></textarea>
-            </div>
-            <div class="flex flex-col space-y-2 w-full">
-                <button title="Create Post" id="create-button"
-                    class="bg-pink-purple text-white px-5 py-3 rounded-xl w-full" type="submit" @click="updatePost">
-                    Update Post
-                </button>
-                <button title="Discard Post"
-                    class="bg-white text-dark px-5 py-3 rounded-xl w-full border border-gray-300"
-                    @click="openDiscardModal">
-                    Discard
-                </button>
             </div>
 
         </div>
 
 
+    </div>
 
-        <ConfirmationModal :showModal=isDeleting :close="closeDeleteModal" :confirm="confirmDelete" header="Woah there!"
-            message="Are you sure you want to delete this post?" />
-        <ConfirmationModal :showModal=isDiscardingUpdate :close="closeDiscardModal" :confirm="confirmDiscard"
-            header="Woah there!" message="Are you sure you want to discard your update?" />
-        <ConfirmationModal :showModal=isDeletingComment :close="closeDeleteCommentModal" :confirm="deleteComment"
-            header="Woah there!" :message="`Are you sure you want to delete this '${commentMsg}'?`" />
+    <div v-if="currentPost&&isEditing"
+        class="flex-1 max-w-4xl bg-gray-100 border border-gray-300 rounded-lg shadow transition-shadow hover:shadow-md p-5 m-2">
+        <div class="form-group w-full mb-4">
+            <label for="title" class="block mb-2 text-gray-700">Title:</label>
+            <input type="text" id="title" required :value="currentPost.postTitle" class="input">
+        </div>
+        <div class="form-group w-full mb-4">
+            <label for="content" class="block mb-2 text-gray-700">Content:</label>
+            <textarea id="content" required :value="currentPost.postBody" class="input"></textarea>
+        </div>
+        <div class="flex flex-col space-y-2 w-full">
+            <button title="Update Post" id="create-button" class="bg-pink-purple text-white px-5 py-3 rounded-xl w-full"
+                type="submit" @click="updatePost">
+                Update Post
+            </button>
+            <button title="Discard Post" class="bg-white text-dark px-5 py-3 rounded-xl w-full border border-gray-300"
+                @click="openDiscardModal">
+                Discard
+            </button>
+        </div>
 
+    </div>
+
+
+
+    <ConfirmationModal :showModal=isDeleting :close="closeDeleteModal" :confirm="confirmDelete" header="Woah there!"
+        message="Are you sure you want to delete this post?" />
+    <ConfirmationModal :showModal=isDiscardingUpdate :close="closeDiscardModal" :confirm="confirmDiscard"
+        header="Woah there!" message="Are you sure you want to discard your update?" />
+    <ConfirmationModal :showModal=isDeletingComment :close="closeDeleteCommentModal" :confirm="deleteComment"
+        header="Woah there!" :message="`Are you sure you want to delete this '${commentMsg}'?`" />
+    <Alert :showModal="showAlert" :header="alertMsg.header" :message="alertMsg.message" :close="closeAlert"  />
 
     </div>
 </template>
