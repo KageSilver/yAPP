53ce78198e8cc7750a078bd8300159fcd0ad6d5f
Qiwen <qzhang0127@gmail.com>
2024-10-27 01:27:42 -0500

    Refactor out BasePostActivity

---
+++ yAppMobile/app/src/main/java/com/example/yappmobile/BasePostActivity.java
@@ -0,0 +1,107 @@
+package com.example.yappmobile;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.materialswitch.MaterialSwitch;
+import com.google.android.material.textfield.TextInputLayout;
+
+import org.json.JSONObject;
+
+public abstract class BasePostActivity extends AppCompatActivity {
+    protected TextInputLayout titleText;
+    protected TextInputLayout contentText;
+    protected MaterialSwitch diaryEntry;
+    protected MaterialSwitch anonymous;
+    protected String postTitle;
+    protected String postBody;
+    protected JSONObject newPost;
+    protected AlertDialog successDialog;
+    protected AlertDialog failureDialog;
+    protected AlertDialog discardDialog;
+
+    protected Button actionButton;
+
+    protected Button discardButton;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the activity to be full screen
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        setContentView(R.layout.activity_create_post);
+
+
+        titleText = findViewById(R.id.post_title);
+        contentText = findViewById(R.id.post_content);
+        diaryEntry = findViewById(R.id.diaryEntry);
+        anonymous = findViewById(R.id.anonymous);
+        actionButton = findViewById(R.id.create_button);
+        discardButton = findViewById(R.id.discard_button);
+
+        EditText titleEditText = titleText.getEditText();
+        EditText contentEditText = contentText.getEditText();
+        titleEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                titleText.setError(null);
+            }
+        });
+        contentEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                contentText.setError(null);
+            }
+        });
+
+    }
+
+    protected void toggleDiaryEntry() {
+        if (diaryEntry.isChecked()) {
+            anonymous.setVisibility(View.VISIBLE);
+        } else {
+            anonymous.setVisibility(View.GONE);
+            anonymous.setChecked(true);
+        }
+    }
+
+    protected boolean hasFilledForms(String postTitle, String postBody) {
+        return !postTitle.equals("") && !postBody.equals("");
+    }
+
+    public abstract  void initializeSuccessDialog() ;
+
+    public  abstract  void initializeFailureDialog();
+    public  abstract  void initializeDiscardDialog();
+
+}
+
+
+
