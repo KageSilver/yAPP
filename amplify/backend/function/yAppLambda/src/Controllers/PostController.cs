bfbc06200b7c445ee7710d9edf4d8efd9da2ff9b
kellyshaye <159831015+kellyshaye@users.noreply.github.com>
2024-10-27 22:39:18 -0500

    Revert changes to comments in PostController

--- amplify/backend/function/yAppLambda/src/Controllers/PostController.cs
+++ amplify/backend/function/yAppLambda/src/Controllers/PostController.cs
@@ -29,24 +29,13 @@ public class PostController : ControllerBase
         _dbContext = dbContext;
         _postActions = postActions;
     }
-
-    /// <code>
-    /// POST: api/posts/createPost with body
-    /// {
-    ///     "uid": "uid",
-    ///     "postBody": "body",
-    ///     "postTitle": "title",
-    ///     "diaryEntry": false,
-    ///     "anonymous": false
-    /// }
-    /// </code>
-    /// ----------------------------------------------------------------------------------------------------------------
+    
+    // POST: api/posts/createPost with body {"uid": "uid", "postBody": "body", "postTitle": "title", "diaryEntry": false, "anonymous": false}
     /// <summary>
     /// Creates a new post.
     /// </summary>
     /// <param name="post">The post object containing the details of the post</param>
     /// <returns>An ActionResult containing the Post object if the request is successful, or an error message if it fails</returns>
-    /// ----------------------------------------------------------------------------------------------------------------
     [HttpPost("createPost")]
     [ProducesResponseType(typeof(Post), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -93,13 +82,11 @@ public class PostController : ControllerBase
     }
 
     // GET: api/posts/getPostById?pid={pid}
-    /// ----------------------------------------------------------------------------------------------------------------
     /// <summary>
     /// Retrieves a post by a unique identifier.
     /// </summary>
     /// <param name="pid">The unique identifier for a post.</param>
     /// <returns>An ActionResult containing the Post object if found, or a NotFound result otherwise</returns>
-    /// ----------------------------------------------------------------------------------------------------------------
     [HttpGet("getPostById")]
     [ProducesResponseType(typeof(Post), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -121,14 +108,12 @@ public class PostController : ControllerBase
     }
 
     // GET: api/posts/getPostsByUser?uid={uid}&diaryEntry={diaryEntry}
-    /// ----------------------------------------------------------------------------------------------------------------
     /// <summary>
     /// Retrieves all posts from a user, either all public posts or all diary entries.
     /// </summary>
     /// <param name="uid">The uid used to find all posts created by a user.</param>
     /// <param name="diaryEntry">If the query is for public posts or diary entries.</param>
     /// <returns>A list of posts created by a user, either public posts or diary entries.</returns>
-    /// ----------------------------------------------------------------------------------------------------------------
     [HttpGet("getPostsByUser")]
     [ProducesResponseType(typeof(List<Post>), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -145,14 +130,12 @@ public class PostController : ControllerBase
     }
     
     // GET: api/posts/getDiariesByUser?uid={uid}&current={current}
-    /// ----------------------------------------------------------------------------------------------------------------
     /// <summary>
     /// Gets the diary entries made by a user within a specific day
     /// </summary>
     /// <param name="uid">The author of the diary entry.</param>
     /// <param name="current">The current day to query.</param>
     /// <returns>The diary entry made by a user on the specified day.</returns>
-    /// ----------------------------------------------------------------------------------------------------------------
     [HttpGet("getDiariesByUser")]
     [ProducesResponseType(typeof(List<Post>), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -173,14 +156,12 @@ public class PostController : ControllerBase
     }
     
     // GET: api/posts/getDiariesByFriends?uid={uid}&current={current}
-    /// ----------------------------------------------------------------------------------------------------------------
     /// <summary>
     /// Gets the diary entries made by the user's friends within a specific day
     /// </summary>
     /// <param name="uid">The user whose friends will be searched for.</param>
     /// <param name="current">The current day to query.</param>
     /// <returns>A list of diary entries made by the user's friends on the specified day</returns>
-    /// ----------------------------------------------------------------------------------------------------------------
     [HttpGet("getDiariesByFriends")]
     [ProducesResponseType(typeof(List<Post>), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -201,14 +182,12 @@ public class PostController : ControllerBase
     }
     
     // GET: api/posts/getRecentPosts?since={since}&maxResults={maxResults}
-    /// ----------------------------------------------------------------------------------------------------------------
     /// <summary>
     /// Gets recent posts from before a specified time.
     /// </summary>
     /// <param name="since">Returns posts made after this time.</param>
     /// <param name="maxResults">The maximum number of results to retrieve.</param>
     /// <returns>A list of recent posts.</returns>
-    /// ----------------------------------------------------------------------------------------------------------------
     [HttpGet("getRecentPosts")]
     [ProducesResponseType(typeof(List<Post>), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -225,13 +204,11 @@ public class PostController : ControllerBase
     }
     
     // DELETE: api/posts/deletePost?pid={pid}
-    /// ----------------------------------------------------------------------------------------------------------------
     /// <summary>
     /// Deletes a post from the database by a post id.
     /// </summary>
     /// <param name="pid">The id of the post to be deleted.</param>
     /// <returns>A boolean indicating whether the deletion was successful.</returns>
-    /// ----------------------------------------------------------------------------------------------------------------
     [HttpDelete("deletePost")]
     [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -246,29 +223,13 @@ public class PostController : ControllerBase
 
         return deleted;
     }
-
-    /// <code>
-    /// PUT: api/posts/updatePost with body
-    /// {
-    ///     "pid": "pid",
-    ///     "createdAt": "createdAt",
-    ///     "updatedAt": "updatedAt",
-    ///     "uid": "uid",
-    ///     "postTitle": "title",
-    ///     "postBody": "body",
-    ///     "upvotes": "upvotes",
-    ///     "downvotes": "downvotes",
-    ///     "diaryEntry": false,
-    ///     "anonymous": false
-    /// }
-    /// </code>
-    /// ----------------------------------------------------------------------------------------------------------------
+    
+    // PUT: api/posts/updatePost with body {"pid": "pid", "createdAt": "createdAt", "updatedAt": "updatedAt", "uid": "uid", "postTitle": "title", "postBody": "body", "upvotes": "upvotes", "downvotes": "downvotes", "diaryEntry": false, "anonymous": false}
     /// <summary>
     /// Edits an already existing post.
     /// </summary>
     /// <param name="request">The new version of the post after editing.</param>
     /// <returns>An ActionResult containing the edited Post object if successful, or an error message if it fails.</returns>
-    /// ----------------------------------------------------------------------------------------------------------------
     [HttpPut("updatePost")]
     [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
