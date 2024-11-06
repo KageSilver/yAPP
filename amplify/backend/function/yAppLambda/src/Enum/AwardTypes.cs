2a6b415e13bfb63fdfd3957317abfb37da8dd2af
Cynthia <calvocyn@gmail.com>
2024-11-06 16:14:23 -0600

    Add enum for different types of awards

---
+++ amplify/backend/function/yAppLambda/src/Enum/AwardTypes.cs
@@ -0,0 +1,11 @@
+namespace yAppLambda.Enum;
+
+/// <summary>
+/// Represents the types of awards
+/// </summary>
+public enum AwardTypes
+{
+    Upvotes = 0,
+    Downvotes = 1,
+    Comments = 2
+}
