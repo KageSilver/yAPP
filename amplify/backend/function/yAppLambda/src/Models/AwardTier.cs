9b7467743d22a20d6b87a7bcb0d900d6fd42a74f
Cynthia <calvocyn@gmail.com>
2024-11-08 17:12:02 -0600

    Add json and models for awards

---
+++ amplify/backend/function/yAppLambda/src/Models/AwardTier.cs
@@ -0,0 +1,15 @@
+using System.Text.Json.Serialization;
+
+namespace yAppLambda.Models;
+
+/// <summary>
+/// Represents a tier of an award that can be earned by a user
+/// </summary>
+public class AwardTier
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("minimum")]
+    public int Minimum { get; set; }
+}
